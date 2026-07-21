cc8b8690d23907ae9bf4a3379b183dd0687ebbfc
Yann Ngateu <96346131+Yan739@users.noreply.github.com>
2026-07-21 04:39:57 +0200

    fix: remove nested git repository in scriptum-api

---
+++ scriptum-api/src/main/java/com/yann/scriptum/ScriptumApplication.java
@@ -0,0 +1,13 @@
+package com.yann.scriptum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptumApplication.class, args);
+    }
+
+}
---
+++ scriptum-api/src/test/java/com/yann/scriptum/ScriptumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yann.scriptum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
