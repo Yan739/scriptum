2cc7e7fa038982044b4662d1767a93cd74a651c9
Yann Ngateu <96346131+Yan739@users.noreply.github.com>
2026-07-22 03:17:35 +0200

    chore(api): enable async processing on the main application class

--- scriptum-api/src/main/java/com/yann/scriptum/ScriptumApplication.java
+++ scriptum-api/src/main/java/com/yann/scriptum/ScriptumApplication.java
@@ -2,7 +2,9 @@ package com.yann.scriptum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class ScriptumApplication {
 
